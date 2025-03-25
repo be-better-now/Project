7e6c5ab300146a507b8b9b60fd397c8fd33ae572
Nguyễn Hữu Duy <huuduy.uwu@gmail.com>
2025-03-25 11:19:46 +0700

    update

---
+++ BookStoreManagement/src/java/duynh/controller/DeleteAccountServlet.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duynh.controller;
+
+import duynh.registration.RegistrationDAO;
+import duynh.registration.RegistrationDTO;
+import duynh.utils.MyApplicationConstants;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+import javax.naming.NamingException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author MSI
+ */
+public class DeleteAccountServlet extends HttpServlet {
+//    private String ERROR_PAGE = "error.html";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        ServletContext context = this.getServletContext();
+        Properties siteMaps = (Properties) context.getAttribute("SITE_MAPS");
+        
+//        String url = ERROR_PAGE;
+        String url = MyApplicationConstants.ApplicationScope.ERROR_PAGE;
+        
+        String username = request.getParameter("pk");
+        String searchValue = request.getParameter("lastSearchValue");
+        
+        try {
+            // 0. Check cust/guest try to use the feature
+            HttpSession session = request.getSession(false);
+            boolean isAdmin = false;
+            if(session!=null) {
+                RegistrationDTO user = (RegistrationDTO) session.getAttribute("USER");
+                if(user!=null) {
+                    isAdmin = user.isRole();
+                }
+            }
+            if(isAdmin==false) {
+//                url = ACCOUNT_FEATURE_CONSTRAINT_ERROR_PAGE;
+                url = MyApplicationConstants.ApplicationScope.ACCOUNT_FEATURE_CONSTRAINT_ERROR_PAGE;
+                return; /// report the error to user
+            }
+            // 1. Call DAO
+            RegistrationDAO dao = new RegistrationDAO();
+            boolean result = dao.deleteAccount(username);
+            // 2. Refresh --> call the Search Function again using URL rewriting tech
+            if(result) {
+//                url = "DispatchController"
+//                        + "?btAction=Search"
+//                        + "&txtSearchValue=" + searchValue;
+                url = MyApplicationConstants.ApplicationScope.SEARCH_LASTNAME_ACTION
+                        + "?txtSearchValue=" + searchValue;
+            }
+        } catch (SQLException ex) {
+            log("DeleteAccountServlet _SQL: " + ex.getMessage());
+        } catch (NamingException ex) {
+            log("DeleteAccountServlet _Naming: " + ex.getMessage());
+        }
+        finally {
+            response.sendRedirect(url);
+            // using for url rewriting
+            /// 1. undercontrol --> duplicated name for parameters
+            /// 2. reduce cost --> ending the request scope
+            
+//            RequestDispatcher rd = request.getRequestDispatcher(url);
+//            rd.forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ BookStoreManagement/src/java/duynh/filter/AuthorizationFilter.java
@@ -0,0 +1,236 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duynh.filter;
+
+import duynh.registration.RegistrationDAO;
+import duynh.registration.RegistrationDTO;
+import duynh.utils.MyApplicationConstants;
+import duynh.utils.SHA256;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.List;
+import javafx.util.Pair;
+import javax.naming.NamingException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author MSI
+ */
+public class AuthorizationFilter implements Filter {
+    
+    private static final boolean debug = false;
+
+    // The filter configuration object we are associated with.  If
+    // this value is null, this filter instance is not currently
+    // configured. 
+    private FilterConfig filterConfig = null;
+    
+    public AuthorizationFilter() {
+    }    
+    
+    private void doBeforeProcessing(ServletRequest request, ServletResponse response)
+            throws IOException, ServletException {
+        if (debug) {
+            log("Authorization:DoBeforeProcessing");
+        }
+        
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        try {
+            // Check logined-user exist in session ~ for case user in a session + url without any parameter + cookie timeout
+            HttpSession session = req.getSession(); // session is existed surely
+            RegistrationDTO user = (RegistrationDTO) session.getAttribute("USER");
+            
+            // 1. Get role
+            Boolean role = null;
+            if (user != null) { // logined
+                role = user.isRole();            
+            }
+
+            /// 2 Authoriztion for user
+            duynh.authorization.Authorization authorization = new duynh.authorization.Authorization();
+            List<Pair<String, String>> featureNavs = authorization.getFeatureNavs(role); /// null for unlogined-user, true for admin, and false for cust
+            session.setAttribute("FEATURE_NAVS", featureNavs);
+            // end of already user acc exist in session
+//            System.out.println("User's request from: " + role);
+        } /*catch (NoSuchAlgorithmException ex) {
+            log("AuthorizationFilter _PASSWORD_HASHING: " + ex.getMessage());
+        } catch (SQLException ex) {
+            log("AuthorizationFilter _SQL: " + ex.getMessage());
+        } catch (NamingException ex) {
+            log("AuthorizationFilter _Naming: " + ex.getMessage());
+        }*/ finally {
+
+        }
+    }    
+    
+    private void doAfterProcessing(ServletRequest request, ServletResponse response)
+            throws IOException, ServletException {
+        if (debug) {
+            log("Authorization:DoAfterProcessing");
+        }
+
+        // Write code here to process the request and/or response after
+        // the rest of the filter chain is invoked.
+        // For example, a logging filter might log the attributes on the
+        // request object after the request has been processed. 
+        /*
+	for (Enumeration en = request.getAttributeNames(); en.hasMoreElements(); ) {
+	    String name = (String)en.nextElement();
+	    Object value = request.getAttribute(name);
+	    log("attribute: " + name + "=" + value.toString());
+
+	}
+         */
+        // For example, a filter might append something to the response.
+        /*
+	PrintWriter respOut = new PrintWriter(response.getWriter());
+	respOut.println("<P><B>This has been appended by an intrusive filter.</B>");
+         */
+    }
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+        if (debug) {
+            log("Authorization:doFilter()");
+        }
+        
+        doBeforeProcessing(request, response);
+        
+        Throwable problem = null;
+        try {
+            chain.doFilter(request, response);
+        } catch (Throwable t) {
+            log("Authorization:doFilter(): " + t.getMessage());
+        }
+        
+        doAfterProcessing(request, response);
+    }
+
+    /**
+     * Return the filter configuration object for this filter.
+     */
+    public FilterConfig getFilterConfig() {
+        return (this.filterConfig);
+    }
+
+    /**
+     * Set the filter configuration object for this filter.
+     *
+     * @param filterConfig The filter configuration object
+     */
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy() {        
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {        
+        this.filterConfig = filterConfig;
+        if (filterConfig != null) {
+            if (debug) {                
+                log("Authorization:Initializing filter");
+            }
+        }
+    }
+
+    /**
+     * Return a String representation of this object.
+     */
+    @Override
+    public String toString() {
+        if (filterConfig == null) {
+            return ("Authorization()");
+        }
+        StringBuffer sb = new StringBuffer("Authorization(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+    
+    private void sendProcessingError(Throwable t, ServletResponse response) {
+        String stackTrace = getStackTrace(t);        
+        
+        if (stackTrace != null && !stackTrace.equals("")) {
+            try {
+                response.setContentType("text/html");
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                PrintWriter pw = new PrintWriter(ps);                
+                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
+
+                // PENDING! Localize this for next official release
+                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");                
+                pw.print(stackTrace);                
+                pw.print("</pre></body>\n</html>"); //NOI18N
+                pw.close();
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        } else {
+            try {
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                t.printStackTrace(ps);
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+    
+    public static String getStackTrace(Throwable t) {
+        String stackTrace = null;
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            t.printStackTrace(pw);
+            pw.close();
+            sw.close();
+            stackTrace = sw.getBuffer().toString();
+        } catch (Exception ex) {
+        }
+        return stackTrace;
+    }
+    
+    public void log(String msg) {
+        filterConfig.getServletContext().log(msg);        
+    }
+    
+}
